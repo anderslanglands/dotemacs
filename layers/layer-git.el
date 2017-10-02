5143a72502da97e8d9aed51de0c5a5d3995f687f
Anders Langlands <anderslanglands@gmail.com>
2017-10-03 08:37:15 +1300

    remove custom magit display function

--- layers/layer-git.el
+++ layers/layer-git.el
@@ -7,6 +7,18 @@
 
 (use-package magit :ensure t
   :config
+;; (setq magit-display-buffer-function
+;;       (lambda (buffer)
+;;         (display-buffer
+;;          buffer (if (and (derived-mode-p 'magit-mode)
+;;                          (memq (with-current-buffer buffer major-mode)
+;;                                '(magit-process-mode
+;;                                  magit-revision-mode
+;;                                  magit-diff-mode
+;;                                  magit-stash-mode
+;;                                  magit-status-mode)))
+;;                     nil
+;;                   '(display-buffer-same-window)))))
   (use-package evil-magit :ensure t)
   )
 
