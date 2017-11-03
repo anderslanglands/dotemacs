84c201484d0bd91d98d4ec248ea1b605ffe98640
Anders Langlands <anderslanglands@gmail.com>
2017-11-03 17:38:28 +1300

    Add CUDA layer

---
+++ layers/layer-cuda.el
@@ -0,0 +1,11 @@
+;;; layer-cuda.el --- CUDA setup
+
+;;; Commentary:
+
+;;; Code:
+(message "Loading layer-cuda...")
+
+(use-package cuda-mode :ensure t)
+
+(provide 'layer-cuda)
+;;; layer-cuda.el ends here
